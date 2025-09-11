3fc1ae03501696efbc21e61e89fb39a3e9601c61
tahiralvi <engtahiralvi@gmail.com>
2025-09-11 11:48:19 -0700

    Initial project added
    Initial project added

---
+++ OnlineNotePad/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineNotePad.Models;
+using System.Collections.Generic;
+
+namespace OnlineNotePad.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Document> Documents { get; set; }
+    }
+}
---
+++ OnlineNotePad/Models/Document.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineNotePad.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        [Required]
+        public string? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+    }
+}
